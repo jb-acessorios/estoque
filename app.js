f34a21041dfbd22d4098f105bc958465163f508d
jb-acessorios <jbacessorios2026@gmail.com>
2026-02-14 16:34:21 -0300

    Update API_URL to new Google Apps Script endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // ==========================================
 
 // COLE A URL DO SEU WEB APP AQUI:
-const API_URL = "https://script.google.com/macros/s/AKfycbwxLABExh5-KwzmzZU8HAF6wj6gbiqVW0pgu8haA9YXvRmGZnezmXzWOses0iirwlU/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyHVcnecp3NFhp4IUn5PGm8COiPjDWUDl089onl7MfB7AidNn7op1J0sIi53AD7-CrMyw/exec";
 
 let TOKEN = "";
 let PRODUCTS = [];
